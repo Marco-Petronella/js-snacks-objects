7f906c8b58513d5a5a1c33d707f833eb455bd367
Marco-Petronella <marco.petronella85@gmail.com>
2026-03-24 13:00:35 +0100

    generate football teams array with fouls and points

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,31 @@ function lighterBicycleCheck() {
   lighterBicycleName.innerHTML = lessWeightName;
   lighterBicycleWeight.innerHTML = "con i suoi " + lessWeight + "Kg";
 }
+
+/*
+Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti
+e stampiamo tutto in console.*/
+
+const footballTeams = [
+  { nome: "Milan" },
+  { nome: "Juventus" },
+  { nome: "Inter" },
+  { nome: "Roma" },
+  { nome: "Napoli" },
+  { nome: "Fiorentina" },
+  { nome: "Pizzighettone" },
+];
+compileFootballTeamsArray()
+
+function compileFootballTeamsArray() {
+    for (let i=0; i<footballTeams.length;i++) {
+        footballTeams[i].point = Math.floor((Math.random()*100));
+        footballTeams[i].fouls = Math.floor((Math.random()*20));
+        console.log(footballTeams[i]);
+    }
+}
