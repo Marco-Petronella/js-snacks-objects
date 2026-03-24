0ce9c95c9ff0eda3e32c7da13460c534ac24f1e1
Marco-Petronella <marco.petronella85@gmail.com>
2026-03-24 12:04:04 +0100

    bicycle array and list function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-// test d
+/*snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+const bicycleList = document.getElementById("bicycle-list");
+const bicycles = [
+  { nome: "Colnago", peso: 3 },
+  { nome: "Bianchi", peso: 2 },
+  { nome: "Trek", peso: 4 },
+  { nome: "Giant", peso: 5 },
+  { nome: "Ducati", peso: 7 },
+];
+
+
+populateBicycleList();
+
+function populateBicycleList() {
+  for (let i = 0; i < bicycles.length; i++) {
+    bicycleList.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
+    ${bicycles[i].nome}<span>${bicycles[i].peso}Kg</span></li>`;
+  }
+}
